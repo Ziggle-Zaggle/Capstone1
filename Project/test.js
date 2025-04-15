d5fa285463504029c55c5dae71d8939c0090f586
Ziggle-Zaggle <zeke.armstrong1@gmail.com>
2025-04-15 11:50:06 -0700

    added test

---
+++ Project/test.js
@@ -0,0 +1,81 @@
+
+document.querySelector('#A1').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results1").textContent= "The Correct Answer is C, You are wrong"
+    document.querySelector("#selected1").textContent= "You Answered A"
+
+
+})
+
+
+document.querySelector('#B1').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results1").textContent= "The Correct Answer is C, You are wrong"
+    document.querySelector("#selected1").textContent= "You Answered B"
+
+
+})
+
+
+document.querySelector('#C1').addEventListener("click", function(){
+   
+    document.querySelector("#results1").textContent= "The Correct Answer is C, You are right"
+    document.querySelector("#selected1").textContent= " You Answered C"
+
+
+})
+
+
+document.querySelector('#D1').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results1").textContent= "The Correct Answer is C, You are wrong"
+    document.querySelector("#selected1").textContent= "You Answered D"
+
+
+})
+document.querySelector('#A2').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results2").textContent= "The Correct Answer is B, You are wrong"
+    document.querySelector("#selected2").textContent= "You Answered A"
+
+
+})
+
+
+document.querySelector('#B2').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results2").textContent= "The Correct Answer is B, You are Correct"
+    document.querySelector("#selected2").textContent= "You Answered B"
+
+
+})
+
+
+document.querySelector('#C2').addEventListener("click", function(){
+   
+    document.querySelector("#results2").textContent= "The Correct Answer is B,You are Wrong"
+    document.querySelector("#selected2").textContent= " You Answered C"
+
+
+})
+
+
+document.querySelector('#D2').addEventListener("click", function(){
+   
+
+
+    document.querySelector("#results2").textContent= "The Correct Answer is B, You are wrong"
+    document.querySelector("#selected2").textContent= "You Answered D"
+
+
+})
